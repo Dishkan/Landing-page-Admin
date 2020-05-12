8ac2989cad93dac4b59006f94f84a9c1402fc296
Dishkan <DH.17@mail.ru>
2020-05-12 21:49:22 +0500

    Landing page

---
+++ order.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Спасибо!</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Заказ на создание лендинга." />
+	<meta name="keywords" content="заказ лендинга, создание лендинга" />
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link type="text/css" rel="stylesheet" href="/styles/order.css" />
+</head>
+<body>
+	<div>
+		<h1>Спасибо!</h1>
+		<p>Через несколько минут с Вами свяжется наш специалист.</p>
+		<p>
+		   <a href="index.php">Вернуться на главную?</a>
+		</p>
+	</div>
+</body>
